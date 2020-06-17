bba9b65c0b077d16856e7eb8c9ab6cc1fe271ada
Tomoya Nagasawa <ocrybit@gmail.com>
2020-06-17 08:22:31 +0200

    auto-copy conf.sample.js to conf.js

--- src/create.js
+++ src/create.js
@@ -1,9 +1,9 @@
 import path from "path"
 import fs from "fs-extra"
-import { exec } from "child_process"
 import { sync as commandExists } from "command-exists"
+import { spawnp } from "./util"
 
-export default dist => {
+export default async dist => {
   const target_path = path.resolve(dist)
   if (fs.existsSync(target_path)) {
     console.error("Error: directory exists.")
@@ -13,18 +13,20 @@ export default dist => {
     process.exit()
   } else {
     const app_path = "https://github.com/warashibe/next-dapp-bare.git"
-    exec(
-      `git clone ${app_path} ${target_path} && rm -rf ${target_path}/.git`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`)
-          return
-        }
-        console.log(
-          `Success: A next-dapp project has been created at ${target_path}.`
-        )
-        process.exit()
-      }
+    const code = await spawnp("git", ["clone", app_path, target_path])
+    if (code !== 0) {
+      console.error(`clone error`)
+      process.exit()
+    }
+    const code2 = await spawnp("rm", ["-rf", `${target_path}/.git`])
+    if (code2 !== 0) {
+      console.error(`.git remove error`)
+      process.exit()
+    }
+    fs.copySync(`${target_path}/nd/conf.sample.js`, `${target_path}/nd/conf.js`)
+    console.log(
+      `Success: A next-dapp project has been created at ${target_path}.`
     )
+    process.exit()
   }
 }
